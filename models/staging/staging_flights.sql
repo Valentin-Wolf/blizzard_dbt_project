489d1610c5b459d3b721d38026721741c12a5612
Valentin Wolf <Valentinwolf@posteo.de>
2024-12-16 17:41:37 +0100

    edits of the table names

--- models/staging/staging_flights.sql
+++ models/staging/staging_flights.sql
@@ -1,6 +1,6 @@
     WITH flights_full_table AS (
         SELECT * 
-        FROM {{source('staging_flights', 'flights')}}
+        FROM {{source('staging_flights', 'flights_all_blizzard')}}
         WHERE DATE_PART('month', flight_date) = 1 
     )
     SELECT * FROM flights_full_table
