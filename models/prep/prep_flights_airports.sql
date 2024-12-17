27dc61a8646c1c08f69f5e33181ed44095a44f49
Valentin Wolf <Valentinwolf@posteo.de>
2024-12-17 10:25:36 +0100

    changed the prep_flights_airport.sql model code for the one from alex

--- models/prep/prep_flights_airports.sql
+++ models/prep/prep_flights_airports.sql
@@ -1,14 +1,42 @@
-WITH origin_data AS (
-		SELECT f.*, a.name AS origin_name, a.city AS origin_city, a.country AS origin_country
-		FROM {{ref('staging_flights')}} AS f
-		LEFT JOIN {{ref('staging_airports')}} AS a
-		ON f.origin = a.faa
-	),
-	dest_data AS (
-		SELECT od.*, a.name AS dest_name, a.city AS dest_city, a.country AS dest_country
-		FROM origin_data AS od
-		LEFT JOIN airports AS a
-		ON od.dest = a.faa
-	)
-	SELECT *
-		FROM dest_data
+WITH flights_data AS (
+    SELECT * 
+    FROM {{ref('staging_flights')}}
+    WHERE dest IN ('BOS','IAD','JFK')
+),
+flights_cleaned AS(
+    SELECT flight_date::DATE
+            ,TO_CHAR(dep_time, 'fm0000')::TIME AS dep_time
+            ,TO_CHAR(sched_dep_time, 'fm0000')::TIME AS sched_dep_time
+            ,dep_delay
+		    ,(dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval
+            ,TO_CHAR(arr_time, 'fm0000')::TIME AS arr_time
+            ,TO_CHAR(sched_arr_time, 'fm0000')::TIME AS sched_arr_time
+            ,arr_delay
+            ,(arr_delay * '1 minute'::INTERVAL) AS arr_delay_interval
+            ,airline
+            ,tail_number
+            ,flight_number
+            ,origin
+            ,dest
+            ,air_time
+            ,(air_time * '1 minute'::INTERVAL) AS air_time_interval
+            ,actual_elapsed_time
+            ,(actual_elapsed_time * '1 minute'::INTERVAL) AS actual_elapsed_time_interval
+            ,(distance / 0.621371)::NUMERIC(6,2) AS distance_km -- see instruction hint
+            ,cancelled
+            ,diverted
+    FROM flights_data
+),
+add_names AS (
+    SELECT tc.* 
+            ,a1.name AS origin_name            
+            ,a1.city AS origin_city
+            ,a1.country AS origin_country
+            ,a2.name AS dest_name
+            ,a2.city AS dest_city
+            ,a2.country AS dest_country
+    FROM flights_cleaned tc
+    INNER JOIN sql_api_group1.airports a1 ON tc.origin = a1.faa 
+    INNER JOIN sql_api_group1.airports a2 ON tc.dest = a2.faa 
+)
+SELECT * FROM add_names
